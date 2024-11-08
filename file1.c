0a5f6e23fbb8b16e80f8680bf3049cf342817715
ketan kashyap <ketankashyap342@gmail.com>
2024-11-08 16:11:31 +0530

    some changes

--- file1.c
+++ file1.c
@@ -7,7 +7,7 @@ int main(){
 =======
 #include <stdio.h>
 int main(){
-    printf(" i am ketan kashyap");
+    printf(" hii i am ketan kashyap");
     return 0;
 }
 >>>>>>> cb3d793cfbbea30ccff961dc7dc9728bf43d7c1e
